882f93c09885e34ca745d86df031f5d89bf203cf
Fernando dos Reis Santos Filho <ferfilho@motorola.com>
2023-09-12 11:49:22 -0300

    feat: implement light spawner

--- src/prius_sdc_pkg/setup.py
+++ src/prius_sdc_pkg/setup.py
@@ -1,19 +1,20 @@
-import os
+from setuptools import setup
+import os 
 from glob import glob
-from setuptools import setup, find_packages
-
 package_name = 'prius_sdc_pkg'
 
 setup(
     name=package_name,
     version='0.1.0',
-    packages=find_packages(),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
-        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+        (os.path.join('share', package_name,'launch'), glob('launch/*')),
+        (os.path.join('share', package_name,'worlds'), glob('worlds/*')),
+        (os.path.join('lib', package_name), glob('scripts/*')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -25,8 +26,9 @@ setup(
     entry_points={
         'console_scripts': [
             'recorder_node = prius_sdc_pkg.video_recorder:main',
-            'driver_node = prius_sdc_pkg.driving_node:main',
-            'spawner_node = prius_sdc_pkg.sdf_spawner:main'
+            'driver_node =   prius_sdc_pkg.driving_node:main',
+            'spawner_node =  prius_sdc_pkg.sdf_spawner:main'
+
         ],
     },
 )
