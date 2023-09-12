a319a3b1bd4dde83f8871a370bf65566ee8a244b
Fernando dos Reis Santos Filho <ferfilho@motorola.com>
2023-09-12 11:49:44 -0300

    feat: finish self driving car gazebo world

--- src/prius_sdc_pkg/launch/world.launch.py
+++ src/prius_sdc_pkg/launch/world.launch.py
@@ -7,7 +7,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     package_dir = get_package_share_directory('prius_sdc_pkg')
-    world_file = os.path.join(package_dir, 'worlds', 'car.world')
+    world_file = os.path.join(package_dir, 'worlds', 'self_driving_car.world')
 
     return LaunchDescription([
         
