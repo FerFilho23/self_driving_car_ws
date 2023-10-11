bddb8e1765cf16829c1053413e11adac99c12b1a
Fernando dos Reis Santos Filho <ferfilho@motorola.com>
2023-10-11 10:51:13 -0300

    feat: lane detection through color segmentation

--- src/self_driving_car_pkg/self_driving_car_pkg/detection/lanes/color_segmentation.py
+++ src/self_driving_car_pkg/self_driving_car_pkg/detection/lanes/color_segmentation.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+from .Morph_op import BwareaOpen, Ret_LowestEdgePoints, RetLargestContour_OuterLane
+
 # Initialize HLS values for white and yellow regions
 hls = 0
 src = 0
@@ -73,6 +75,40 @@ cv2.createTrackbar("Hue_Low", "yellow_regions", low_H_yellow, 255, on_hue_low_ch
 cv2.createTrackbar("Lit_Low", "yellow_regions", low_L_yellow, 255, on_lit_low_change_yellow)
 cv2.createTrackbar("Sat_Low", "yellow_regions", low_S_yellow, 255, on_sat_low_change_yellow)
 
+def get_mask_and_edge_of_larger_objects(frame,mask,min_area):
+    # Keeping only objects larger then min_area
+    frame_roi = cv2.bitwise_and(frame,frame,mask=mask)
+    frame_roi_gray = cv2.cvtColor(frame_roi,cv2.COLOR_BGR2GRAY)
+    mask_of_larger_objects = BwareaOpen(frame_roi_gray,min_area)
+    frame_roi_gray = cv2.bitwise_and(frame_roi_gray,mask_of_larger_objects)
+    # Extracting Edges of those larger objects
+    frame_roi_smoothed = cv2.GaussianBlur(frame_roi_gray,(11,11),1)
+    edges_of_larger_objects = cv2.Canny(frame_roi_smoothed,50,150, None, 3)
+
+    return mask_of_larger_objects,edges_of_larger_objects
+
+def segment_midlane(frame, white_regions, min_area):
+    mid_lane_mask, mid_lane_edge = get_mask_and_edge_of_larger_objects(frame, white_regions, min_area)
+    return mid_lane_mask, mid_lane_edge
+
+def segment_outerlane(frame, yellow_regions, min_area):
+    outer_points_list = []
+    mask, edges = get_mask_and_edge_of_larger_objects(frame, yellow_regions, min_area)
+
+    mask_largest, largest_found = RetLargestContour_OuterLane(mask, min_area)
+
+    if largest_found:
+        # Keep only edges of largest region
+        edge_largest = cv2.bitwise_and(edges, mask_largest)
+
+        # Return edge points for identifying closest edge later
+        lanes_sides_sep, outer_points_list = Ret_LowestEdgePoints(mask_largest)
+        edges = edge_largest
+    else:
+        lanes_sides_sep = np.zeros((frame.shape[0], frame.shape[1]), np.uint8)
+
+    return edges, lanes_sides_sep, outer_points_list
+
 # Function to segment lanes based on color
 def segment_lanes(frame, min_area):
     global hls, src
@@ -82,11 +118,11 @@ def segment_lanes(frame, min_area):
     hls = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)
 
     # Define HLS color range values for white and yellow regions
-    lower_white = (low_H_white, low_L_white, low_S_white)
-    upper_white = (255, 255, 255)
+    lower_white = np.array([low_H_white, low_L_white, low_S_white])
+    upper_white =  np.array([255, 255, 255])
 
-    lower_yellow = (low_H_yellow, low_L_yellow, low_S_yellow)
-    upper_yellow = (255, 255, 255)
+    lower_yellow =  np.array([low_H_yellow, low_L_yellow, low_S_yellow])
+    upper_yellow =  np.array([255, 255, 255])
 
     # Create masks for white and yellow regions
     white_regions = cv2.inRange(hls, lower_white, upper_white)
@@ -96,3 +132,12 @@ def segment_lanes(frame, min_area):
     cv2.imshow("white_regions", white_regions)
     cv2.imshow("yellow_regions", yellow_regions)
     cv2.waitKey(1)
+
+    # Semgneting midlane from white regions
+    mid_lane_mask,mid_lane_edge = segment_midlane(frame,white_regions,min_area)
+
+    # Semgneting outerlane from yellow regions
+    outer_lane_edge,outerlane_side_sep,outerlane_points = segment_outerlane(frame,yellow_regions,min_area+500)        
+
+    return mid_lane_mask,mid_lane_edge,outer_lane_edge,outerlane_side_sep,outerlane_points
+    
--- src/self_driving_car_pkg/self_driving_car_pkg/detection/lanes/lane_detection.py
+++ src/self_driving_car_pkg/self_driving_car_pkg/detection/lanes/lane_detection.py
@@ -1,3 +1,4 @@
+import cv2
 from .color_segmentation import segment_lanes
 from ...config import config
 
@@ -5,4 +6,9 @@ def detect_lanes(frame):
     # Cropping the img (keep only the street that is below the horizon)
     img_cropped = frame[config.CropHeight_resized:,:]
 
-    segment_lanes(img_cropped, config.minArea_resized)
+    mid_lane_mask,mid_lane_edge,outer_lane_edge,outerlane_side_sep,outerlane_points = segment_lanes(img_cropped,config.minArea_resized)
+    cv2.imshow("mid_lane_mask",mid_lane_mask)
+    cv2.imshow("mid_lane_edge",mid_lane_edge)
+    cv2.imshow("outer_lane_edge",outer_lane_edge)
+    cv2.imshow("outerlane_side_sep",outerlane_side_sep)
+    cv2.waitKey(1)
