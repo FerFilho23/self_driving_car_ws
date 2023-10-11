8aaee5e2d57c06659076c5624fa93ea928f49588
Fernando dos Reis Santos Filho <ferfilho@motorola.com>
2023-10-11 12:01:48 -0300

    feat: midlane estimation

--- src/self_driving_car_pkg/self_driving_car_pkg/detection/lanes/lane_detection.py
+++ src/self_driving_car_pkg/self_driving_car_pkg/detection/lanes/lane_detection.py
@@ -1,5 +1,6 @@
 import cv2
 from .color_segmentation import segment_lanes
+from .midlane_estimation import estimate_midlane
 from ...config import config
 
 def detect_lanes(frame):
@@ -7,8 +8,11 @@ def detect_lanes(frame):
     img_cropped = frame[config.CropHeight_resized:,:]
 
     mid_lane_mask,mid_lane_edge,outer_lane_edge,outerlane_side_sep,outerlane_points = segment_lanes(img_cropped,config.minArea_resized)
+    estimated_midlane = estimate_midlane(mid_lane_edge, config.MaxDist_resized)
+
     cv2.imshow("mid_lane_mask",mid_lane_mask)
     cv2.imshow("mid_lane_edge",mid_lane_edge)
     cv2.imshow("outer_lane_edge",outer_lane_edge)
     cv2.imshow("outerlane_side_sep",outerlane_side_sep)
+    cv2.imshow("estimated_midlane", estimated_midlane)
     cv2.waitKey(1)
